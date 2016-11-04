86652b4feaf2bae98b53cf7f43eb060cbae21a7b
Roslyn <roz@axworks.com>
2016-11-03 20:27:31 -0700

    fix style for + unary operator

--- nuleaf-source/database/steminars.js
+++ nuleaf-source/database/steminars.js
@@ -31,8 +31,8 @@ exports.find = function(conditions, callback) {
     conditions = {};
   }
 
-  var skip = + conditions.skip;
-  var limit = + conditions.limit;
+  var skip = +conditions.skip;
+  var limit = +conditions.limit;
 
   delete conditions.skip;
   delete conditions.limit;
--- nuleaf-source/database/teams.js
+++ nuleaf-source/database/teams.js
@@ -27,8 +27,8 @@ exports.find = function(conditions, callback) {
     conditions = {};
   }
 
-  var skip = + conditions.skip;
-  var limit = + conditions.limit;
+  var skip = +conditions.skip;
+  var limit = +conditions.limit;
 
   delete conditions.skip;
   delete conditions.limit;
